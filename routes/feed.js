12b0b597ba09da296a5b8eed1dc8ba67b538c1d7
unknown <seanhill2016@gmail.com>
2018-12-09 14:29:15 -0500

    added routes for skeleton

---
+++ routes/feed.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const users = require("../data/users");
+
+router.get("/", async (req, res) => {
+	const sid = req.cookies.AuthCookie;
+	let user = null;
+	try {
+		user = await users.getUserBySession(sid);
+	} catch (e) {
+		throw (e);
+	}
+	
+	(user == null ? auth=true : auth=false)
+
+	if (auth == false) {
+		let data = {
+			title: "feed"
+		}
+		res.render("feed", data);
+	} else {
+		let data = {
+			title: "Error 403",
+			issue: "You are not logged in."
+		}
+		res.render("error", data);
+	}
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@ const landingRoute = require("./landing");
 const loginRoute = require("./login");
 const privateRoute = require("./private");
 const logoutRoute = require("./logout");
+const feedRoute = require("./feed");
+const winnerRoute = require("./winner");
+const votingRoute = require("./voting");
+
 
 function constructorMethod(app) {
 	app.use("/", landingRoute);
+	app.use("/voting", votingRoute);
+	app.use("/winner", winnerRoute);
+	app.use("/feed", feedRoute);
 	app.use("/login", loginRoute);
 	app.use("/private", privateRoute);
 	app.use("/logout", logoutRoute);
