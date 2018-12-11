bfaa313505e430d221608d7c569e685965a8b744
David Kim <polandgod123@gmail.com>
2018-12-11 15:08:07 -0500

    Revert "trying to convert URL to base64"
    This reverts commit 031667bb5111e2a1c87d3d3dd46d9e5014450c3c.

--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const users = require("../data/users");
-var fs = require('fs');
 
 router.get("/", async (req, res) => {
-	let topic = "Dog";
-
 	const sid = req.cookies.AuthCookie;
 	let user = null;
 	try {
@@ -18,8 +15,7 @@ router.get("/", async (req, res) => {
 
 	if (auth == true) {
 		let data = {
-			title: "FEED",
-			FormLabel: `Submit a photo! TOPIC: ${topic}`
+			title: "feed"
 		}
 		res.render("feed", data);
 	} else {
@@ -28,33 +24,6 @@ router.get("/", async (req, res) => {
 			issue: "You are not logged in."
 		}
 		res.render("error", data);
-	}
-});
-// function getBase64Image(img) {
-// 	var canvas = document.createElement("canvas");
-// 	canvas.width = img.width;
-// 	canvas.height = img.height;
-// 	var ctx = canvas.getContext("2d");
-// 	ctx.drawImage(img, 0, 0);
-// 	var dataURL = canvas.toDataURL("image/png");
-// 	return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-//   }
-function getBase64Image(img) {
-    var bitmap = fs.readFileSync(img);
-    return new Buffer(bitmap).toString('base64');
-}
-  
-router.post("/", async(req,res) => {
-	try {
-		console.log("HAPPENING");
-		let test = req.body.pic;
-		var base64 = getBase64Image(req.body.pic);
-		console.log(base64);
-		console.log("ENDING");
-		res.render("feed",{FormLabel: "Upload Complete!"});
-	}
-	catch(e) {
-		console.log(e);
 	}
 });
 
