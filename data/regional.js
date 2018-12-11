9809f6a226135dd6c11dd2c6e63ac6f1e13b4eaf
Philip Cho <pcho2@stevens.edu>
2018-12-11 13:43:46 -0500

    Adding all area data files

--- data/regional.js
+++ data/regional.js
@@ -1,30 +1,30 @@
 const mongoCollections = require("../config/mongoCollections");
-const local = mongoCollections.local;
+const regional = mongoCollections.regional;
 const users = require("./users");
 const gallery = require("./gallery");
 const uuid = require("node-uuid");
 
 const exportedMethods = {
 
-  async getLocalById(id) {
-    const localCollection = await local();
-    const localPost = await localCollection.findOne({ _id: id });
+  async getRegionalById(id) {
+    const regionalCollection = await regional();
+    const regionalPost = await regionalCollection.findOne({ _id: id });
 
-    if (!localPost) throw "Post not found";
-    return localPost;
+    if (!regionalPost) throw "Post not found";
+    return regionalPost;
   },
-  async addLocalPost( topic, pid, userId) {
+  async addRegionalPost( topic, pid, userId) {
       //Create ID?
     if (typeof topic !== "string") throw "No topic provided";
     if (typeof pid !== "string") throw "No pid provided";
     if (typeof pid !== "string") throw "No userId provided";
 
 
-    const localCollection = await local();
+    const regionalCollection = await regional();
 
     const userThatPosted = await users.getUserById(userId);
     const galleryThatPosted = await gallery.getGalleryById(pid);
-    const newLocal = {
+    const newRegional = {
     _id: uuid.v4(),
     creator : {
         name : userThatPosted.name,
@@ -33,23 +33,23 @@ const exportedMethods = {
     },
     votes : 0,
     topic : topic,
-    location : userThatPosted.profile.local,
+    location : userThatPosted.profile.regional,
     pid : pid
     };
 
-    const newInsertInformation = await localCollection.insertOne(newLocal);
+    const newInsertInformation = await regionalCollection.insertOne(newRegional);
     const newId = newInsertInformation.insertedId;
     return await this.getPostById(newId);
   },
-  async removePostLocal(id) {
-    const localCollection = await local();
-    const deletionInfo = await localCollection.removeOne({ _id: id });
+  async removePostRegional(id) {
+    const regionalCollection = await regional();
+    const deletionInfo = await regionalCollection.removeOne({ _id: id });
     if (deletionInfo.deletedCount === 0) {
       throw `Could not delete post with id of ${id}`;
     }
   },
-  async upvotePostLocal(id) {
-    area = "local";
+  async upvotePostRegional(id) {
+    area = "regional";
     const updatedVotes = await gallery.upvotePost(id, area);
     return updatedVotes;
   }
