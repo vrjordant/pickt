07c42339c770e950b31da0fb50339b43999e57d1
Gabby Padriga <gpadriga@stevens.edu>
2018-12-12 13:03:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const configRoutes = require("./routes");
 const exphbs = require("express-handlebars");
 const cookieParser = require("cookie-parser");
 const userData = require("./data/users.js");
+const static = express.static(__dirname + "/css");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+app.use("/css", static);
+app.use(express.static('resources'));
 
 app.use(cookieParser());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
