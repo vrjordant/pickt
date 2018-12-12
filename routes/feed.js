52f739514c19290cd2461f458e082034dd1d0a77
Philip Cho <pcho2@stevens.edu>
2018-12-11 20:01:12 -0500

    Revert "ignore this"
    This reverts commit 98f3c0df337f3421df7ef2519278949ed0e5add1.

--- routes/feed.js
+++ routes/feed.js
@@ -26,59 +26,10 @@ router.get("/", async (req, res) => {
 		res.render("error", data);
 	}
 });
-
-
-
-/**
- * Convert an image 
- * to a base64 url
- * @param  {String}   url         
- * @param  {Function} callback    
- * @param  {String}   [outputFormat=image/png]           
- */
-// function convertImgToBase64URL(url, callback, outputFormat){
-	function convertImgToBase64URL(url, outputFormat){
-    var img = new Image();
-    img.crossOrigin = 'Anonymous';
-    img.onload = function(){
-        var canvas = document.createElement('CANVAS'),
-        ctx = canvas.getContext('2d'), dataURL;
-        canvas.height = img.height;
-        canvas.width = img.width;
-        ctx.drawImage(img, 0, 0);
-        dataURL = canvas.toDataURL(outputFormat);
-        // callback(dataURL);
-        canvas = null; 
-    };
-	img.src = url;
-	return dataURL;
-}
-
-
-function getBase64Image(img) {
-	var canvas = document.createElement("canvas");
-	canvas.width = img.width;
-	canvas.height = img.height;
-	var ctx = canvas.getContext("2d");
-	ctx.drawImage(img, 0, 0);
-	var dataURL = canvas.toDataURL("image/png");
-	return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-  }
-  
-
-
-
 router.post("/", async (req, res) => {
-	console.log("Hello");
 	try {
-		console.log("Start");
-		// let test = req.body.pic;
-		// res.render(feed,{LabelForm: "Upload Completed!"});
-		// var testy = convertImgToBase64URL(test,'image');
-		// var testy2 = getBase64Image(test);
-		// console.log(test);
-		// console.log(testy);
-		console.log("End");
+		let test = req.body.pic;
+		res.render(feed,{LabelForm: "Upload Completed!"});
 	}
 	catch (e) {
 		console.log(e);
